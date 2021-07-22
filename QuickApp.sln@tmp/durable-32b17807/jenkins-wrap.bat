d9e5406e55629987d8484e08a59e263deb15a848
Minh.Phan81299 <51442765+minhphan81299@users.noreply.github.com>
2021-07-22 15:24:27 +0700

    Delete jenkins-wrap.bat

--- QuickApp.sln@tmp/durable-32b17807/jenkins-wrap.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-cmd /c call "C:\Users\Admin\.jenkins\workspace\QuickApp\QuickApp.sln@tmp\durable-32b17807\jenkins-main.bat" > "C:\Users\Admin\.jenkins\workspace\QuickApp\QuickApp.sln@tmp\durable-32b17807\jenkins-log.txt" 2>&1
-echo %ERRORLEVEL% > "C:\Users\Admin\.jenkins\workspace\QuickApp\QuickApp.sln@tmp\durable-32b17807\jenkins-result.txt"
